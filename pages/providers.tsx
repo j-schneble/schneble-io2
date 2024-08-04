7436d27bc4a797a29b9948fa58a2ef9d24884b51
j-schneble <schneble.jack@gmail.com>
2024-08-04 16:27:43 -0400

    theme provider removal

--- pages/providers.tsx
+++ pages/providers.tsx
@@ -2,18 +2,17 @@
 
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+
 
 export interface ProvidersProps {
 	children: React.ReactNode;
-	themeProps?: ThemeProviderProps;
+
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export function Providers({ children }: ProvidersProps) {
 	return (
 		<NextUIProvider>
-			<NextThemesProvider disableTransitionOnChange attribute='class' {...themeProps}>{children}</NextThemesProvider>
+			{children}
 		</NextUIProvider>
 	);
 }
