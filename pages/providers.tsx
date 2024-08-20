375b990ccd3791fe370dcab9245fad06c34dc738
jack schneble <jackschneble@jacks-MacBook-Air.local>
2024-08-20 14:03:49 -0400

    router provider

--- pages/providers.tsx
+++ pages/providers.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import * as React from "react";
-import { NextUIProvider } from "@nextui-org/system";
+import {NextUIProvider} from "@nextui-org/react";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+import {ThemeProviderProps} from "next-themes/dist/types";
 import {useRouter} from "next/navigation";
 
 export interface ProvidersProps {
-	children: React.ReactNode;
-
+  children: React.ReactNode;
+  themeProps?: ThemeProviderProps;
 }
 
-export function Providers({ children }: ProvidersProps) {
-	const router = useRouter();
+export function Providers({children, themeProps}: ProvidersProps) {
+  const router = useRouter();
 
-	return (
-		<NextUIProvider>
-			<div>
-			{children}
-			</div>
-		</NextUIProvider>
-	)
+  return (
+    <NextUIProvider navigate={router.push}>
+      <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+    </NextUIProvider>
+  );
 }
